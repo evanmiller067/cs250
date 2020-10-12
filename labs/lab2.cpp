5391b5237caef8ad363cf11cbfdf0dea1e6cfbd8
Evan Miller <evanmiller067@gmail.com>
2020-10-12 15:32:14 -0700

    starting lab 2

---
+++ labs/lab2.cpp
@@ -0,0 +1,17 @@
+/*
+NAME: Evan Miller
+ASSIGNMENT: Lab 2
+SOURCES:
+https://www.geeksforgeeks.org/how-to-return-multiple-values-from-a-function-in-c-or-cpp/
+https://stackoverflow.com/questions/5029840/convert-char-to-int-in-c-and-c
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+return 0;
+}
