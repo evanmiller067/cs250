c00bb61d0561c90c2a04deeda3d54f451d3ceb64
Evan Miller <evanmiller067@gmail.com>
2020-11-13 21:22:36 -0800

    updating lab3

--- labs/lab3.cpp
+++ labs/lab3.cpp
@@ -35,68 +35,85 @@ int main()
 	mpz_set_ui(q,0);
 	mpz_set(r,n1);
 
-	//compute
+	//division algorithm
 	while(mpz_cmp(r,n2)>= 0)
 	{
 		mpz_sub(r,r,n2);
 		mpz_add_ui(q,q,1);
 	}
-	
-	//output
 	cout << "\nQuotient: ";
 	mpz_out_str(stdout,10,q);
 	cout << "\nRemainder: ";
 	mpz_out_str(stdout,10,r);
 	cout << endl;
-
+	
+	
 //------------------------------
 //Using mpz function to test
+/*
 	cout << "GCD using built-in function: ";
 	mpz_t ans;
 	mpz_init(ans);
 	mpz_gcd(ans, n1, n2);
 	mpz_out_str(stdout, 10, ans);
 	cout << endl;
+*/
 //------------------------------
 
-	cout << "\nGCD Using Euclidean Algorithm: " << endl;
-	mpz_t dividen, divisor, gcdResult;
+	cout << "\nGCD: " << endl;
+	mpz_t dividen, divisor, gcdResult, remainder;
 	mpz_init(dividen);
 	mpz_init(divisor);
 	mpz_init(gcdResult);
+	mpz_init(remainder);
 
+	if(n1 == 0)
+	{
+		mpz_set(gcdResult,n2);
+	}
+	if(n2 == 0)
+	{
+		mpz_set(gcdResult,n1);
+	}
 	if(n1 > n2)
 	{
 		mpz_set(divisor, n2);
 		mpz_set(dividen, n1);
 	}
-	else
+	if(n1 < n2)
 	{
 		mpz_set(divisor, n1);
 		mpz_set(dividen, n2);
 	}
-	mpz_fdiv_q(q, dividen, divisor);
-	mpz_mod(r, dividen, divisor);
+	mpz_cdiv_q(q, dividen, divisor);
+	mpz_mod(remainder, dividen, divisor);
 	mpz_set(gcdResult,divisor);
-	while(r > 0)
+	while(remainder > 0 || remainder < 0)
 	{
 		mpz_set(dividen,divisor);
-		mpz_set(divisor,r);
-		mpz_fdiv_q(q, dividen, divisor);
-		mpz_mod(r, dividen, divisor);
-		if(r == 0)
-		{
-			mpz_set(gcdResult,divisor);
-		}
+		mpz_set(divisor,remainder);
+		mpz_cdiv_q(q, dividen, divisor);
+		mpz_mod(remainder, dividen, divisor);
 	}
+	if(remainder == 0)
+	{
+		mpz_set(gcdResult,divisor);
+	}
+	mpz_set(gcdResult, divisor);
+	//output
 	mpz_out_str(stdout, 10, gcdResult);
+	cout << "\nQuotient: ";
+	mpz_out_str(stdout,10,q);
+	cout << "\nRemainder: ";
+	mpz_out_str(stdout,10,r);
+	cout << endl;
 	
 	//clear big nums
 	mpz_clear(n1);
 	mpz_clear(n2);
 	mpz_clear(q);
 	mpz_clear(r);
-	mpz_clear(ans);
+//	mpz_clear(ans);
 	mpz_clear(dividen);
 	mpz_clear(divisor);
 	mpz_clear(gcdResult);
